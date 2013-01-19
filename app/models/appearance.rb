f976f5e73e7439165d1ddd7a8772a397d70061c1
Stuart Swindells <github@lumo.me.uk>
2013-01-19 22:01:51 +0000

    Add many-to-many link between the Person and Appearance models

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,3 +1,5 @@
 class Appearance < ActiveRecord::Base
   attr_accessible :date
+
+  has_and_belongs_to_many :people
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,5 @@
 class Person < ActiveRecord::Base
   attr_accessible :name
+  
+  has_and_belongs_to_many :appearances
 end
