fd519f7c7a386a0b3fae643699c6f30d6216a82e
Stuart Swindells <github@lumo.me.uk>
2013-01-19 22:02:00 +0000

    Validations

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,5 +1,7 @@
 class Appearance < ActiveRecord::Base
   attr_accessible :date
-
+  
+  validates :date, presence: true, uniqueness: true
+  
   has_and_belongs_to_many :people
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,7 @@
 class Person < ActiveRecord::Base
   attr_accessible :name
   
+  validates :name, presence: true
+  
   has_and_belongs_to_many :appearances
 end
