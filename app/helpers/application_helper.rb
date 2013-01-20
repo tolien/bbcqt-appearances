0edebb4c702c43116e62f7fab3d167b792f6fee6
Stuart Swindells <github@lumo.me.uk>
2013-01-20 23:44:35 +0000

    Add a method to help populate the database

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  
+  def load_appearance(date, people)
+    puts date.class
+    puts people.class
+    
+    people = people.split(',')
+    
+    appearance = Appearance.create(date: date)
+    if not appearance.id
+      appearance = Appearance.find_by_date(appearance.date)
+    end
+    
+    people.each do |name|
+      name = name.strip
+      person = Person.find_by_name(name)
+      
+      if not person
+        person = Person.create(name: name)
+      end
+      
+      person.appearances << appearance
+    end
+  end
 end
