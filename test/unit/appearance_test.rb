74c5c39fc37c1863a11606bd192246012c90d3a2
Stuart Swindells <github@lumo.me.uk>
2013-01-20 15:34:12 +0000

    Unit tests

--- test/unit/appearance_test.rb
+++ test/unit/appearance_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class AppearanceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Required attributes" do
+    appearance = Appearance.new
+    assert appearance.invalid?
+    assert appearance.errors[:date].any?
+    
+    appearance.date = '17/1/2013'
+    assert appearance.valid?
+  end
+  
+  test "Date validation" do
+    appearance = appearances(:one)
+    assert appearance.valid?
+    
+    appearance.date = "29/2/2013"
+    assert appearance.invalid?
+  end
+  
+  test "Unique appearances" do
+    one = Appearance.create(date: '17/1/2013')
+    two = Appearance.create(date: '17/1/2013')
+    
+    assert one.valid?
+    assert two.invalid?
+  end
 end
--- test/unit/person_test.rb
+++ test/unit/person_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "A person can only be at an appearance once" do
+    nick = people(:nick)
+    one = appearances(:one)
+    nick.appearances << one
+    assert nick.valid?
+    
+    nick.appearances << one
+    assert_equal 1, nick.appearances.size
+  end
 end
