17c8e516bff6e40ebac7f4cec9685b9cc1e23fbf
Stuart Swindells <github@lumo.me.uk>
2013-01-20 15:35:14 +0000

    Validate appearance date and allow a person to appear once on a date

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,7 +1,7 @@
 class Appearance < ActiveRecord::Base
   attr_accessible :date
   
-  validates :date, presence: true, uniqueness: true
+  validates :date, presence: true, uniqueness: true, timeliness: {type: :date}
   
   has_and_belongs_to_many :people
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -3,5 +3,9 @@ class Person < ActiveRecord::Base
   
   validates :name, presence: true
   
-  has_and_belongs_to_many :appearances
+  has_and_belongs_to_many :appearances, uniq: true, :before_add => :only_one_at_an_appearance
+  
+  def only_one_at_an_appearance(appearance)
+    raise ActiveRecord::Rollback if self.appearances.exists? appearance
+  end
 end
