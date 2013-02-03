d952cd836c716a676f3a29739ae20ce3b97fe58c
Stuart Swindells <github@lumo.me.uk>
2013-02-03 01:41:50 +0000

    Use Capistrano for deployment

---
+++ Capfile
@@ -0,0 +1,6 @@
+require 'secret_token_replacer/recipes'
+
+load 'deploy'
+# Uncomment if you are using Rails' asset pipeline
+     load 'deploy/assets'
+load 'config/deploy' # remove this line to skip loading any of the default tasks
---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+#require 'secret_token_replacer/recipes'
+
+set :application, "bbcqt-appearances"
+set :repository,  "https://github.com/tolien/bbcqt-appearances"
+set :svm, :git
+set :deploy_to, "/var/sites/#{application}"
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+#role :app, "your app-server here"                          # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+server "ra.tolien.co.uk", :app, :web, :db, :primary => true
+set :use_sudo, false
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+  task :seed do
+    run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
+  end
+end
+
+after "deploy:cold", "deploy:secrets"
